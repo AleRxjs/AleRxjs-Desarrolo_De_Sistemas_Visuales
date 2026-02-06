76d4c1927fb1d7a240f345b0dda04c0d4079567b
Alejandro <cuh512410278@cuh.mx>
2026-02-06 10:17:10 -0600

    Borrar

--- Cliente/Proyectoo_Vite/practica1-api-notas/server/src/modelos/nota.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const notaSchema = new mongoose.Schema({
-    titulo: { type: String, required: true },
-    contenido: { type: String, required: true },
-    fecha: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Nota", notaSchema);
